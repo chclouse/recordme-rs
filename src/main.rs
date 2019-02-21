f899961e8387ace33c8e660465a762ef514dbd35
Christopher Clouse <chc2t@mtmail.mtsu.edu>
2019-02-21 14:34:30 -0600

    Changed frrom discord-rs to serenity.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 extern crate ini;
-extern crate discord;
+extern crate serenity;
 
 use ini::Ini;
-use discord::Discord;
-use discord::model::Event;
+use serenity::{
+    client::{Client, EventHandler},
+    prelude::Context,
+    model::channel::Message
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, _ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(e) = msg.reply("*pong*") {
+                println!("[WARN] Failed to send message: {:?}", e);
+            }
+        }
+    }
+}
 
 fn main() {
     // Login and establish connection.
     let conf = Ini::load_from_file("conf.ini").unwrap();
-    let client = Discord::from_bot_token(&conf["discord"]["token"]).expect("Login failed.");
-    let (mut conn, _) = client.connect().expect("Connection failed.");
+    let mut client = Client::new(&conf["discord"]["token"], Handler).unwrap();
 
-    // Here's where stuff would normally be done.
-    loop {
-        let event = conn.recv_event().expect("Could not recieve events.");
-        if let Event::MessageCreate(message) = event {
-            if message.content == "!ping" {
-                match client.send_message(message.channel_id, "*pong*", "", false) {
-                    Err(e) => println!("[WARN] Failed to send message: {:?}", e),
-                    _ => {}
-                }
-            }
-        }
+    if let Err(e) = client.start() {
+        println!("[FATAL] The bot encountered an error: {:?}", e);
     }
 }
